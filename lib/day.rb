e2bede3111c146086150312c2128f98fdd8e17d2
Ayumi Fukuda Bennett <ayumi.fukuda@gmail.com>
2015-04-26 07:46:06 -0500

    some day/zeller tests passing

--- lib/day.rb
+++ lib/day.rb
@@ -1,5 +1,40 @@
 class Day
 attr_reader :month, :year
 
+  def initialize(month, year)
+    @month = month
+    @year = year
+  end
+###########################################################
+#   ZELLER'S CONGRUENCE                                 
+#  http://en.wikipedia.org/wiki/Zeller's_congruence
+#
+#  m = the month(3=March, 4=April.....13=January, 14=February
+#  y = year
+#
+#  will return the first day of the month:
+#  0=Saturday, 1=Sunday...6=Friday
+###########################################################
+  def feb_and_jan
+    if @month == 1 || @month == 2
+      m = @month + 12
+      y = @year - 1
+    else
+      m = @month + 1
+      y = @year
+    end
+  end
+
+  def day_of_week
+    m = feb_and_jan
+    day = 1
+    q = day
+    y = feb_and_jan
+    d = y / 4
+    c = y / 100
+    f = y / 400
+
+    weekday_name = (q + ((m * 26)/10).floor + y + d.floor + (6 * c).floor + f.floor) % 7
+  end
 
 end
--- test/test_day.rb
+++ test/test_day.rb
@@ -3,5 +3,35 @@ require_relative '../lib/day.rb'
 
 class TestDay < Minitest::Test
 
-  
+  def test_saving_args
+    d = Day.new(01, 2012)
+    assert_equal 1, d.month
+    assert_equal 2012, d.year
+  end
+
+  def test_first_day_of_april_2012
+    d = Day.new(4,2012)
+    expected = 1
+    assert_equal expected, d.day_of_week
+  end
+
+  def test_first_day_of_january_1945
+    d = Day.new(1, 1945)
+    expected = 2
+    assert_equal expected, d.day_of_week
+  end
+
+  def test_first_day_of_february_1952
+    d = Day.new(2, 1952)
+    expected = 6
+    assert_equal expected, d.day_of_week
+  end
+
+  def test_first_day_of_january_01_2000
+    d = Day.new(1, 2000)
+    expected = 0
+    assert_equal expected, d.day_of_week
+  end
+
+
 end
