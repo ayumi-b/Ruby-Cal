34fd7b1b0058a218718f83578b7620230968e2b1
Ayumi Fukuda Bennett <ayumi.fukuda@gmail.com>
2015-05-01 13:48:15 -0500

    added arguments to cal.rb

--- cal.rb
+++ cal.rb
@@ -1,9 +1,21 @@
 #!/usr/bin/env ruby
 
-require_relative 'helper.rb'
+require_relative 'lib/month.rb'
+require_relative 'lib/day.rb'
+require_relative 'lib/year.rb'
 
-month = ARGV[0]
-year = ARGV[1]
 
-m = Month.new(month, year)
-puts m.to_s
+if ARGV.length == 1
+  year = ARGV[0]
+
+  y = Year.new(year)
+  puts y.to_y_s
+elsif ARGV.length == 2
+  month = ARGV[0]
+  year = ARGV[1]
+
+  m = Month.new(month, year)
+  puts m.to_s
+
+end
+
